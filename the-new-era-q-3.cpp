30069ab85a19c4af35953395afcf24a3033ec5c4
shitalshiyani <127853794+shitalshiyani@users.noreply.github.com>
2023-04-28 03:48:25 -0700

    Update the-new-era-q-3.cpp

--- the-new-era-q-3.cpp
+++ the-new-era-q-3.cpp
@@ -11,33 +11,14 @@ class Time
 };
 main()
 {
-	Time t,t1,t2;
-	cout<<"Enter First Time="<<endl;
-	cout<<"Enter Time in Hour=";
-	cin>>t1.hour;
-	cout<<"Enter Time in Minute=";
-	cin>>t1.minute;
-	cout<<"Enter Time in Second=";
-	cin>>t1.second;
+	Time t;
+	cout<<"Enter Time in Seconds=";
+	cin>>t.second;
 	
-	cout<<"Enter Second Time="<<endl;
-	cout<<"Enter Time in Hour=";
-	cin>>t2.hour;
-	cout<<"Enter Time in Minute=";
-	cin>>t2.minute;
-	cout<<"Enter Time in Second=";
-	cin>>t2.second;
+	t.hour=t.second/3600;
+	t.minute=(t.second%3600)/60;
+	t.second=(t.second%3600)%60;
     
-    
-    t.second=t1.second+t2.second;
-    t.minute=t1.minute+t2.minute+(t.second/60);
-    t.hour=t1.hour+t2.hour+(t.minute/60);
-    
-    t.minute%=60;
-    t.second%=60;
-    
-    cout<<"The Final Time is=";
-    cout<<t.hour<<":"<<t.minute<<":"<<t.second;
-    
-	
+	cout<<"The Final Time is=";
+        cout<<t.hour<<":"<<t.minute<<":"<<t.second;	
 }
